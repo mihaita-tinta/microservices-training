5427c4e1ef836a0c2eadb57152b24f67f34a04a4
Mihaita Tinta <mihaita.tinta@ing.com>
2022-11-04 09:21:38 +0200

    lesson 1 hint

--- lesson 1/lab/invoice-api/src/main/java/com/mih/training/invoice/convert/InvoiceXmlConverter.java
+++ lesson 1/lab/invoice-api/src/main/java/com/mih/training/invoice/convert/InvoiceXmlConverter.java
@@ -1,12 +1,21 @@
 package com.mih.training.invoice.convert;
 
 import com.mih.training.invoice.model.InvoiceXml;
+import com.mih.training.invoice.repository.Invoice;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -22,6 +31,22 @@ public class InvoiceXmlConverter implements Converter<File, InvoiceXml> {
 
     @Override
     public InvoiceXml convert(File source) {
+        // FIXME deserialize Invoice and validate
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Invoice.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            Invoice invoice = null;
+
+            SchemaFactory factory =
+                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(xsd);
+            Validator validator = schema.newValidator();
+
+        } catch (JAXBException | SAXException e) {
+            log.warn("convert - invalid XML, exception: " + e.getMessage(), e);
+            throw new IllegalArgumentException("Invalid xml");
+        }
         return null;
     }
 
